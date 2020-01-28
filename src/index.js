e7138aa32aeb5d573db0cc007b2b963fef3b5103
alexander.burkov <alexander.burkov@jetbrains.com>
2020-01-29 01:23:04 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,85 +2,100 @@ const program = require('commander');
 const chalk = require('chalk');
 const fs = require('fs');
 const shell = require('shelljs');
+const axios = require('axios');
+const dayjs = require('dayjs');
 
-let token;
+let accessToken;
 program.version('1.0.0');
 program.name('branch-status');
 program.arguments('<token>');
+program.option('--skip-released', `don't show issues in Released state`, false);
 program.action((arg) => {
-    token = arg;
+    accessToken = arg;
 });
 program.parse(process.argv);
 
+const logError = message => {
+    console.log(chalk.red(`[ERROR] ${message}`));
+};
 
-function tokenMissingBanner() {
-    console.log(chalk.red('[ERROR] Please provide your token to access YouTack!'));
+const tokenMissingBanner = () => {
+    logError('Please provide your token to access YouTack!');
     console.log('See: https://www.jetbrains.com/help/youtrack/incloud/Manage-Permanent-Token.html');
     console.log(`Example: ${program.name()} perm:AMGuaGHuTGlua593.UMAtMTY2MA==.hkjh123123JJLKjl`);
     process.exit(255);
-}
+};
 
-function tryReadFromFile() {
-    return fs.readFileSync('.token', { encoding: 'utf8' });
-}
-
-if (token === undefined) {
-    try {
-        token = tryReadFromFile();
-    } catch (e) {
-        tokenMissingBanner();
-        console.log(chalk.red('[ERROR] Failed to read token from file .token'));
-        process.exit(255);
+const readToken = () => {
+    if (accessToken === undefined) {
+        try {
+            accessToken = fs.readFileSync('.token', { encoding: 'utf8' });
+        } catch (e) {
+            tokenMissingBanner();
+            logError('Failed to read token from file .token');
+            process.exit(255);
+        }
     }
-}
+};
 
-const {code, stdout} = shell.exec('git branch -r --sort=committerdate', { silent: true, fatal: true });
-if(code !== 0) {
-    console.log(chalk.red('[ERROR] Failed to run git branch'));
-}
+const colorizeState = (state) => {
+    const map = {
+        'Implemented': chalk.green,
+        'To be discussed': chalk.red,
+        'Wait for Reply': chalk.red,
+        'In Progress': chalk.yellow,
+        'Reopened': chalk.yellow,
+        'Incomplete': chalk.yellow,
+        'Without Verification': chalk.blue,
+        'Under Verification': chalk.blue,
+        'Verified': chalk.blue,
 
 
-console.log(gitBranchOutput);
+    };
+    const func = map[state.trim()] ? map[state.trim()] : (e) => e;
+    return func(state);
+};
 
 
-// ISSUES=($( | grep -i -o 'JPF-[0-9]\+'))
-// ACCESS_TOKEN=$1
-// API_URL="https://youtrack.jetbrains.com/api"
-//
-// # Convert epoch millis to readable date yyyy-MM-dd
-// function toHumanReadableDate() {
-//     if [ "$1" == "null" ]; then
-//     echo "----------"
-// else
-//     date -r "$(($1 / 1000))" +'%Y-%m-%d'
-//     fi
-// }
-//
-// # Perform GET-request to the specified URL
-// function doGetRequest() {
-//     curl -s -X GET \
-//     -H "Authorization:Bearer $ACCESS_TOKEN" \
-//     -H "Accept:application/json" \
-//     -H "Content-Type:application/json" \
-//     -H "Cache-Control:no-cache" \
-//     "$1"
-// }
-//
-// function formatString() {
-//     printf '%-20s' "$1"
-// }
-//
-// echo "Fetching YT issue statuses for ${#ISSUES[@]} branches..."
-//
-// for issueId in "${ISSUES[@]}"; do
-//     URL="$API_URL/issues/${issueId}?fields=summary,resolved,customFields(id,projectCustomField(field(name)),value(name))"
-//
-//     FIELD_VALUES=$(doGetRequest "$URL")
-// summary=$(echo "$FIELD_VALUES" | jq '.summary' | tr -d '\"')
-// resolvedEpoch=$(echo "$FIELD_VALUES" | jq '.resolved')
-// resolvedDate=$(toHumanReadableDate "$resolvedEpoch")
-// state=$(echo "$FIELD_VALUES" | jq '.customFields[] | select (.id == "123-1006") | .value.name' | tr -d '\"')
-// stateFormatted=$(formatString "$state")
-//
-// echo -e "[$issueId]\tResolved: $resolvedDate\tState: $stateFormatted Summary: $summary"
-// done
+const main = async () => {
+    readToken();
+    const { code, stdout } = shell.exec('git branch -r --sort=committerdate', { silent: true, fatal: true });
+    if (code !== 0) {
+        logError('Failed to run git branch');
+        process.exit(1);
+    }
+
+    const issues = stdout
+        .split('\n')
+        .map((l) => {
+            let result = /(JPF-[0-9]+)/.exec(l);
+            return result ? result[1] : undefined;
+        })
+        .filter((e) => e !== undefined);
+
+    console.log(chalk.green(`Fetching YT issue statuses for ${issues.length} branches...`));
+
+    for (const id of issues) {
+        const {
+            data: {
+                resolved,
+                customFields,
+                summary,
+            },
+        } = await axios.get(`https://youtrack.jetbrains.com/api/issues/${id}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            params: {
+                fields: 'summary,resolved,customFields(id,projectCustomField(field(name)),value(name))',
+            },
+        });
+        const resDate = resolved ? dayjs(resolved).format('YYYY-MM-DD') : '----------';
+        let stateRaw = customFields.find((e) => e.id === '123-1006').value.name;
+        if (stateRaw === 'Released' && program.skipReleased) continue;
+        const state = colorizeState(stateRaw.padEnd(20));
+        console.log(`[${id.padEnd(9)}]  ${resDate}  ${state}  ${summary}`);
+    }
+};
+
+main();
