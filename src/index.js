65c868f7a4d5c161ec4d0907152252063be944dc
alexander.burkov <alexander.burkov@jetbrains.com>
2020-02-10 23:53:41 +0300

    Apply autoformat

--- src/index.js
+++ src/index.js
@@ -14,130 +14,130 @@ program.arguments('<token>');
 program.option('--skip-released', `don't show issues in Released state`, false);
 program.option('--no-sort', 'do not sort', false);
 program.action((arg) => {
-    accessToken = arg;
+  accessToken = arg;
 });
 program.parse(process.argv);
 
 const logError = (message, exitCode = undefined) => {
-    console.log(chalk.red(`[ERROR] ${message}`));
-    if (exitCode !== undefined) process.exit(exitCode);
+  console.log(chalk.red(`[ERROR] ${message}`));
+  if (exitCode !== undefined) process.exit(exitCode);
 };
 
 const appConfDir = `${process.env.HOME}/.config/branch-status`;
 
 const readToken = () => {
-    if (accessToken === undefined) {
-        try {
-            accessToken = fs.readFileSync(`${appConfDir}/.token`, { encoding: 'utf8' }).trim();
-        } catch (e) {
-            logError('Please provide your token to access YouTack!');
-            console.log('See: https://www.jetbrains.com/help/youtrack/incloud/Manage-Permanent-Token.html');
-            console.log(`Example: ${program.name()} perm:AMGuaGHuTGlua593.UMAtMTY2MA==.hkjh123123JJLKjl`);
-            logError('Failed to read token from file .token', 255);
-        }
+  if (accessToken === undefined) {
+    try {
+      accessToken = fs.readFileSync(`${appConfDir}/.token`, { encoding: 'utf8' }).trim();
+    } catch (e) {
+      logError('Please provide your token to access YouTack!');
+      console.log('See: https://www.jetbrains.com/help/youtrack/incloud/Manage-Permanent-Token.html');
+      console.log(`Example: ${program.name()} perm:AMGuaGHuTGlua593.UMAtMTY2MA==.hkjh123123JJLKjl`);
+      logError('Failed to read token from file .token', 255);
     }
+  }
 };
 
 const loadColorMap = () => {
-    const configFile = `${appConfDir}/config.js`;
-    const defaultColorMapFunction = function (chalk) {
-        return {
-            'Implemented': chalk.green,
-            'To be discussed': chalk.red,
-            'Wait for Reply': chalk.red,
-            'In Progress': chalk.yellow,
-            'Reopened': chalk.yellow,
-            'Incomplete': chalk.yellow,
-            'Without Verification': chalk.blue,
-            'Under Verification': chalk.blue,
-            'Verified': chalk.blue,
-        };
+  const configFile = `${appConfDir}/config.js`;
+  const defaultColorMapFunction = function (chalk) {
+    return {
+      'Implemented': chalk.green,
+      'To be discussed': chalk.red,
+      'Wait for Reply': chalk.red,
+      'In Progress': chalk.yellow,
+      'Reopened': chalk.yellow,
+      'Incomplete': chalk.yellow,
+      'Without Verification': chalk.blue,
+      'Under Verification': chalk.blue,
+      'Verified': chalk.blue,
     };
-    if (!fs.existsSync(configFile)) {
-        console.log(chalk.yellow(`Now you can adjust output colors in config file: `) + configFile);
-        fs.mkdirSync(appConfDir, { recursive: true });
-        fs.writeFileSync(configFile, `// find available colors at: https://github.com/chalk/chalk#readme
+  };
+  if (!fs.existsSync(configFile)) {
+    console.log(chalk.yellow(`Now you can adjust output colors in config file: `) + configFile);
+    fs.mkdirSync(appConfDir, { recursive: true });
+    fs.writeFileSync(configFile, `// find available colors at: https://github.com/chalk/chalk#readme
 module.exports = {
     makeColorMap: ${defaultColorMapFunction.toString()}
 };
 `);
-    }
+  }
 
-    try {
-        const { makeColorMap } = require(configFile);
-        return makeColorMap(chalk);
-    } catch (e) {
-        logError(`Failed to load color map from config file, using default one.`)
-        return defaultColorMapFunction(chalk);
-    }
+  try {
+    const { makeColorMap } = require(configFile);
+    return makeColorMap(chalk);
+  } catch (e) {
+    logError(`Failed to load color map from config file, using default one.`);
+    return defaultColorMapFunction(chalk);
+  }
 };
 
 const colorMap = loadColorMap();
 
 const colorizeState = (state) => {
-    const found = colorMap[state.trim()];
-    return found ? found(state) : state;
+  const found = colorMap[state.trim()];
+  return found ? found(state) : state;
 };
 
 
 const main = async () => {
-    readToken();
-    const { code, stdout } = shell.exec('git branch -r --sort=committerdate', { silent: true, fatal: true });
-    if (code !== 0) {
-        logError('Failed to run git branch', code);
-    }
+  readToken();
+  const { code, stdout } = shell.exec('git branch -r --sort=committerdate', { silent: true, fatal: true });
+  if (code !== 0) {
+    logError('Failed to run git branch', code);
+  }
 
-    const issues = stdout
-        .split('\n')
-        .map((l) => {
-            let result = /(JPF-[0-9]+)/.exec(l);
-            return result ? result[1] : undefined;
-        })
-        .filter((e) => e !== undefined);
+  const issues = stdout
+    .split('\n')
+    .map((l) => {
+      let result = /(JPF-[0-9]+)/.exec(l);
+      return result ? result[1] : undefined;
+    })
+    .filter((e) => e !== undefined);
 
-    console.log(chalk.green(`Fetching YT issue statuses for ${issues.length} branches...`));
-    program.sort = !program.noSort;
+  console.log(chalk.green(`Fetching YT issue statuses for ${issues.length} branches...`));
+  program.sort = !program.noSort;
 
-    const result = [];
+  const result = [];
 
-    const ora = new Ora({
-        text: 'Fetching data',
+  const ora = new Ora({
+    text: 'Fetching data',
+  });
+
+  if (program.sort) ora.start();
+
+  for (const [ i, id ] of issues.entries()) {
+    if (program.sort) ora.text = `(${i}/${issues.length}) Fetching issue ${id}`;
+    const {
+      data: {
+        resolved,
+        customFields,
+        summary,
+      },
+    } = await axios.get(`https://youtrack.jetbrains.com/api/issues/${id}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        fields: 'summary,resolved,customFields(id,projectCustomField(field(name)),value(name))',
+      },
     });
+    const resDate = resolved ? dayjs(resolved).format('YYYY-MM-DD') : '----------';
+    const state = customFields.find((e) => e.id === '123-1006').value.name;
+    if (state === 'Released' && program.skipReleased) continue;
+    const stateColorized = colorizeState(state.padEnd(20));
+    const formatted = `[${id.padEnd(9)}]  ${resDate}  ${stateColorized}  ${summary}`;
+    if (program.noSort) console.log(formatted);
+    else result.push({ state, resolved, formatted });
+  }
 
-    if (program.sort) ora.start();
-
-    for (const [ i, id ] of issues.entries()) {
-        if (program.sort) ora.text = `(${i}/${issues.length}) Fetching issue ${id}`;
-        const {
-            data: {
-                resolved,
-                customFields,
-                summary,
-            },
-        } = await axios.get(`https://youtrack.jetbrains.com/api/issues/${id}`, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-            params: {
-                fields: 'summary,resolved,customFields(id,projectCustomField(field(name)),value(name))',
-            },
-        });
-        const resDate = resolved ? dayjs(resolved).format('YYYY-MM-DD') : '----------';
-        const state = customFields.find((e) => e.id === '123-1006').value.name;
-        if (state === 'Released' && program.skipReleased) continue;
-        const stateColorized = colorizeState(state.padEnd(20));
-        const formatted = `[${id.padEnd(9)}]  ${resDate}  ${stateColorized}  ${summary}`;
-        if (program.noSort) console.log(formatted);
-        else result.push({ state, resolved, formatted });
-    }
-
-    if (program.sort) {
-        ora.text = 'All required data fetched';
-        ora.succeed();
-        result
-            .sort((a, b) => a.state.localeCompare(b.state) || (a.resolved - b.resolved))
-            .forEach(({ formatted }) => console.log(formatted));
-    }
+  if (program.sort) {
+    ora.text = 'All required data fetched';
+    ora.succeed();
+    result
+      .sort((a, b) => a.state.localeCompare(b.state) || (a.resolved - b.resolved))
+      .forEach(({ formatted }) => console.log(formatted));
+  }
 };
 
 main();
