519269cda230f0264f69d4dbce0f210d504741d1
Alexander Burkov <alexander.burkov@jetbrains.com>
2022-06-14 14:51:01 +0300

    fix path join

--- source/token.ts
+++ source/token.ts
@@ -1,11 +1,12 @@
 import * as fs from 'fs';
 import * as os from 'os';
+import path from 'path';
 
-export const appConfDir = `${os.homedir()}/.config/branch-status`;
+export const appConfDir = path.join(os.homedir(), '.config', 'branch-status');
 
 const tokenFromConfig = () => {
 	try {
-		return fs.readFileSync(`${appConfDir}/.token`, { encoding: 'utf8' }).trim();
+		return fs.readFileSync(path.join(appConfDir, '.token'), { encoding: 'utf8' }).trim();
 	} catch (e) {}
 	return '';
 };
